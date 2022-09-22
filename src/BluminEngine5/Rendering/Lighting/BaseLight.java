a83f28a96751b8c2a74d30d9ca9a13642a77320d
vrglab <vrglab1@gmail.com>
2022-09-22 14:56:26 +0200

    PostRpocessing and Texture partial transparency + Basic lighting

---
+++ src/BluminEngine5/Rendering/Lighting/BaseLight.java
@@ -0,0 +1,41 @@
+package BluminEngine5.Rendering.Lighting;
+
+import BluminEngine5.Componant.IComponent;
+
+public class BaseLight extends IComponent {
+
+    @Override
+    public void Update() {
+
+    }
+
+    @Override
+    public void OnRender() {
+
+    }
+
+    @Override
+    public void Init() {
+
+    }
+
+    @Override
+    public void PreInit() {
+
+    }
+
+    @Override
+    public void OnExit() {
+
+    }
+
+    @Override
+    public void SceneLoad() {
+
+    }
+
+    @Override
+    public void Destroy() {
+
+    }
+}
